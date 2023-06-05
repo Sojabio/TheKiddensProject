1d3e0234c278630f018d764194e187a22a80fcf8
soja <solene.jabaud@gmail.com>
2023-06-05 05:43:03 +0200

    validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
   has_many :cart_items
   has_many :carts, through: :cart_items
+
+  validates :title, :description, :price, presence: true
+
+  validates :title, uniqueness: true
+
+  validates :price, numericality: { greater_than: 0 }
+
+  #rajouter les images en obligatoire dans la validation
+
+
+
 end
