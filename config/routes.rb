34c649909454f8a527d93214d11a8c504211a05b
Washer34 <antoine.paturel34@free.fr>
2023-06-08 12:46:33 +0200

    Creation admin + peaufinage V2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations'}
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -8,6 +8,12 @@ Rails.application.routes.draw do
     member do
       post 'add_to_cart'
     end
+  end
+  
+  namespace :admin do
+    root 'orders#index'
+    resources :orders
+    resources :items
   end
 
   resources :carts
--- db/schema.rb
+++ db/schema.rb
@@ -94,6 +94,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_133242) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.boolean "is_admin", default: false
+    t.string "name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
