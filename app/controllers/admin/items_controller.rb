a96d31ca2d1246c971e7363f651d4ad062aefb9d
Laurie2023 <129828541+Laurie2023@users.noreply.github.com>
2023-06-08 17:03:58 +0200

    Front_admin + Readme + routes admin

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,14 +1,65 @@
 class Admin::ItemsController < ApplicationController
   before_action :require_admin
   
+  def index
+    @items = Item.friendly.all
+  end 
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new 
+    @item = Item.new
+  end 
+
+  def create
+    @item = Item.new(item_params)
+    @item.photo.attach(params[:item][:photo])
+    if @item.save
+      redirect_to @item, notice: "Le produit à été créé avec succès."
+    else
+      flash[:alert] = @item.errors.full_messages.join(", ")
+      redirect_to new_admin_item_path
+    end
+  end 
+
+  def edit
+    @item = Item.find(params[:id])
+  end 
+
+  def update    
+    if @item.update(item_params)
+      if params[:item][:photo].present?
+        @item.photo.attach(params[:item][:photo])
+      end
+      redirect_to admin_items_path, notice: 'Produit mis à jour avec succès.'
+    else
+      flash[:alert] = @user.errors.full_messages.join(", ")
+      render :edit
+    end    
+  end 
+
+  def chabracadabra
+    @item = Item.friendly.find(params[:id])
+    @item.is_hidden = !@item.is_hidden
+    @item.save
+    redirect_to admin_items_path
+  end
+
   
   private
   
+
   def require_admin
     unless current_user && current_user.is_admin?
       flash[:alert] = "Accès non autorisé"
       redirect_to root_path
     end
+  end
+  
+  def item_params
+    params.require(:item).permit(:title, :description, :price, :photo)
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
+  root "items#index"
   devise_for :users, controllers: { registrations: 'users/registrations'}
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "items#index"
   resources :items do
     member do
       post 'add_to_cart'
     end
   end
-  
+
   namespace :admin do
-    root 'orders#index'
-    resources :orders
-    resources :items
+    resources :items do
+      member do
+        post 'chabracadabra'
+      end
+    end
   end
 
   resources :carts
@@ -21,6 +23,11 @@ Rails.application.routes.draw do
   resources :users
   resources :cart_items, except: [:show]
 
+  namespace :admin do
+    root 'admins#show'
+    resources :orders
+    resources :items
+  end
   scope '/checkout' do
     post 'create', to: 'checkout#create', as: 'checkout_create'
     get 'success', to: 'checkout#success', as: 'checkout_success'
